5d02fc9f0389c393c5430e6fdaa25aa937ff3bcc
01luyicheng <01luyicheng@gmail.com>
2026-06-15 17:23:47 +0800

    fix(mqtt): use tokenSnapshot instead of authToken in startHeartbeat call

--- android/app/src/main/java/com/netproxy/gateway/connection/MqttConnectionManager.kt
+++ android/app/src/main/java/com/netproxy/gateway/connection/MqttConnectionManager.kt
@@ -451,7 +451,7 @@ class MqttConnectionManager @Inject constructor(
 
                 // N80: 先触发已注册的 topicCallbacks，再执行默认订阅
                 // 这样 VpnService 中通过 subscribe() 注册的 disconnect 监听器会被保留
-                startHeartbeat(deviceId, authToken, generation)
+                startHeartbeat(deviceId, tokenSnapshot, generation)
 
                 } catch (e: Exception) {
                     val clientToClose = localClient
